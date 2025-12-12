b12dd34000bbb8204ed59054c881c8b47e9ff501
JustinMoFeng <3309113612@qq.com>
2025-12-13 00:23:24 +0800

    BatchNorm

--- CNN/__init__.py
+++ CNN/__init__.py
@@ -3,3 +3,4 @@ from .AlexNet import AlexNet_main
 from .VGG import vgg_main
 from .NiN import NiN_main
 from .GoogLeNet import GoogLeNet_main
+from .BatchNorm import BatchNorm_LeNet_main, Simple_BatchNorm_LeNet_main
