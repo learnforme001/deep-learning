af6fdfb77508d74dcc6283db83d87cddedd467a4
JustinMoFeng <3309113612@qq.com>
2026-01-10 11:42:58 +0800

    LSTM

--- RNN/__init__.py
+++ RNN/__init__.py
@@ -2,3 +2,4 @@ from .sequence_model import sequence_model_main
 from .RNN_basic import RNN_head
 from .RNN_simple import RNN_torch
 from .GRU import gru_head, gru_torch
+from .LSTM import LSTM_head, LSTM_torch
